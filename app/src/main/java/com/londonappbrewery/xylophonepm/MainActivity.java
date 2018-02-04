0433a1140a074675f8e6c832162f4077fd8e59ed
ayush13mishra <ayush13misha@gmail.com>
2018-02-04 17:10:31 +0530

    Final Version Of Application

--- app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private int mGSoundId;
     private int mASoundId;
     private int mBSoundId;
+    private SoundPool msoundpool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +33,56 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO: Create a new SoundPool
 
+         msoundpool= new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS, AudioManager.STREAM_MUSIC, 0);
 
         // TODO: Load and get the IDs to identify the sounds
+        mCSoundId = msoundpool.load(getApplicationContext(), R.raw.note1_c, 1);
+        mDSoundId = msoundpool.load(getApplicationContext(), R.raw.note2_d, 1);
+        mESoundId = msoundpool.load(getApplicationContext(), R.raw.note3_e, 1);
+        mFSoundId = msoundpool.load(getApplicationContext(), R.raw.note4_f, 1);
+        mGSoundId = msoundpool.load(getApplicationContext(), R.raw.note5_g, 1);
+        mASoundId = msoundpool.load(getApplicationContext(), R.raw.note6_a, 1);         //use soundpool feature for short duraton sounds
+        mBSoundId = msoundpool.load(getApplicationContext(), R.raw.note7_b, 1);
 
 
     }
 
-    // TODO: Add the play methods triggered by the buttons
 
+        // TODO: Add the play methods triggered by the buttons
+
+    public void play1(View v) {
+        msoundpool.play(mCSoundId,1.0f,1.0f,0,0,1.0f);
+    }
+
+    public void play2(View v) {
+        msoundpool.play(mDSoundId,1.0f,1.0f,0,0,1.0f);
+
+    }
+
+    public void play3(View v) {
+        msoundpool.play(mESoundId,1.0f,1.0f,0,0,1.0f);
+
+    }
+
+    public void play4(View v) {
+        msoundpool.play(mFSoundId,1.0f,1.0f,0,0,1.0f);
+
+    }
+
+    public void play5(View v) {
+        msoundpool.play(mGSoundId,1.0f,1.0f,0,0,1.0f);
+
+    }
+
+    public void play6(View v) {
+        msoundpool.play(mASoundId,1.0f,1.0f,0,0,1.0f);
+
+    }
+
+    public void play7(View v) {
+        msoundpool.play(mBSoundId,1.0f,1.0f,0,0,1.0f);
+
+    }
 
 
 }
